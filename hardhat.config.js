2b5f97c6d066f1ed95994426613c6720d0ca21f1
iwipwq <iwipwq@gmail.com>
2022-09-01 21:53:58 +0900

    mod config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,10 +8,12 @@ require("solidity-coverage");
 require("dotenv").config();
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
